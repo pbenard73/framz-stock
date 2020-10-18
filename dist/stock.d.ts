19ceed0d75a50646fd06dbc8ab7bb30422a71e49
BENARD Patrick <pbenard73@gmail.com>
2020-10-18 09:49:37 +0200

    first commit

---
+++ dist/stock.d.ts
@@ -0,0 +1,5 @@
+export const StockModule: typeof MyStockModule;
+declare class MyStockModule {
+    name: string;
+}
+export {};
---
+++ src/stock.js
@@ -0,0 +1,7 @@
+import { Module } from "framz"
+
+class MyStockModule extends Module {
+    name = "stock"
+}
+
+export const StockModule = MyStockModule
